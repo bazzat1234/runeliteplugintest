b4fb40151d5c25d36877e3737b21a163ae5d671b
bazzat1234 <71994751+bazzat1234@users.noreply.github.com>
2021-07-22 07:25:40 +1000

    Initial commit
    Signed-off-by: bazzat1234 <byronthompson.4@hotmail.com>

--- src/main/java/com/lootbeams/LootBeamsConfig.java
+++ src/main/java/com/lootbeams/LootBeamsConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.lootbeams;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface LootBeamsConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
